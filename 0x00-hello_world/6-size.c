3d1448e0c342e904606dfd69ede7f232ba752038
feya <fatimetousidiheiba@gmail.com>
2023-02-16 18:06:29 -0800

    Chart size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,16 @@
 */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of an int: %lu byte(s)\n", sizeof(char));
-printf("Size of a long int: %lu byte(s)\n", sizeof(char));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(char));
-printf("Size of a float: %lu byte(s)\n", sizeof(char));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
