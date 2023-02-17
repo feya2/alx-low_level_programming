d3a2d8546091b92a9d82df93f4457e231b89f35d
feya <fatimetousidiheiba@gmail.com>
2023-02-16 17:24:56 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main -a code using puts
  *
- * Return: Always 0 (Success)
- */
+ * Return: 0
+*/
 
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-            return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
