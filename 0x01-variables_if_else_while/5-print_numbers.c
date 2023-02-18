b627ec52ab6d7c8cf66347e065a462171a6008de
feya <fatimetousidiheiba@gmail.com>
2023-02-17 17:45:19 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <time.h>
-"include <stdlib.h>
+#include <stdlib.h>
 /**
 * main -  all single digit numbers of base 10 starting from 0
 * followed by a new line
@@ -10,7 +10,7 @@ int main(void)
 {
 	char c = '0';
 
-	while (c .= '9')
+	while (c <= '9')
 	{
 		putchar(c);
 		c++;
