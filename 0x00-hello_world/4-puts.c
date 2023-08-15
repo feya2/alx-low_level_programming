58559e9081acef9a1dfd9bc6b0c0ea5d41014189
feya <fatimetousidiheiba@gmail.com>
2023-08-14 19:18:17 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void);
+/**
+ * main - hello
+ *
+ * Description: my first put
+ *
+ * Return: always 0 (succes)
+*/
+
+int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
 return (0);
